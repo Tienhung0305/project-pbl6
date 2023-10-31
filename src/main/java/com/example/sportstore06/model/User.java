3e9cd1d1e6ba8c1287b8f9e39370825fb4b56f6e
VuTien Hung <Tienhungg0305@gmai.com>
2023-10-31 10:43:47 +0700

    10:43:10312023

--- src/main/java/com/example/sportstore06/model/User.java
+++ src/main/java/com/example/sportstore06/model/User.java
@@ -27,35 +27,35 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @NotBlank(message = "name must not be blank")
-    @Size(min = 1, max = 50, message = "name must be between 1 and 50 characters")
+    @NotBlank
+    @Size(min = 1, max = 50)
     private String name;
-    @NotBlank(message = "email must not be blank")
-    @Size(min = 5, max = 50, message = "email must be between 5 and 50 characters")
-    @Email(message = "email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @NotBlank
+    @Size(min = 5, max = 50)
+    @Email(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     private String email;
     private Timestamp dob;
-    @NotBlank(message = "phone number must not be blank")
-    @Size(min = 7, max = 11, message = "phone number must be between 7 and 11 characters")
+    @NotBlank
+    @Size(min = 7, max = 11)
     private String phone;
-    @NotBlank(message = "citizen identification card must not be blank")
-    @Size(min = 10, max = 12, message = "citizen identification card must be between 10 and 12 characters")
+    @NotBlank
+    @Size(min = 10, max = 12)
     private String cic;
-    @NotBlank(message = "address must not be blank")
-    @Size(min = 5, max = 50, message = "address must be between 5 and 50 characters")
+    @NotBlank
+    @Size(min = 5, max = 50)
     private String address;
-    @NotBlank(message = "username must not be blank")
-    @Size(min = 1, max = 50, message = "username must be between 1 and 50 characters")
+    @NotBlank
+    @Size(min = 1, max = 50)
     private String username;
-    @NotBlank(message = "password must not be blank")
-    @Size(min = 1, max = 100, message = "password must be between 1 and 100 characters")
+    @NotBlank
+    @Size(min = 1, max = 100)
     private String password;
     private String remember_token;
     private Timestamp created_at;
     private Timestamp updated_at;
-    @NotNull(message = "state must not be null")
-    @Min(value = 0, message = "state must is (0,1,2)")
-    @Max(value = 3, message = "state must is (0,1,2)")
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 3)
     private Integer state;
 
     @ManyToMany(fetch = FetchType.EAGER)
