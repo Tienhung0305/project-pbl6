c7e96f812f9a364eef05f5ae73a4dcc8364a30e1
VuTien Hung <Tienhungg0305@gmai.com>
2023-11-20 15:43:24 +0700

    update deploy 04

--- src/main/java/com/example/sportstore06/model/Group.java
+++ src/main/java/com/example/sportstore06/model/Group.java
@@ -13,7 +13,7 @@ import java.util.Set;
 @Getter
 @Setter
 @Builder
-@Table(name = "group_category")
+@Table(name = "groups")
 public class Group {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
