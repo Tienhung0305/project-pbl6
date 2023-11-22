bedce6600314f3d0c771bc9a4052128e2efbd9f0
VuTien Hung <Tienhungg0305@gmai.com>
2023-11-22 23:22:11 +0700

    change deploy

--- src/main/java/com/example/sportstore06/dao/response/BusinessResponse.java
+++ src/main/java/com/example/sportstore06/dao/response/BusinessResponse.java
@@ -1,15 +1,12 @@
 package com.example.sportstore06.dao.response;
 
 import com.example.sportstore06.model.Business;
-import com.example.sportstore06.model.Product;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Timestamp;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -20,13 +17,11 @@ public class BusinessResponse {
     private String name;
     private String about;
     private Integer tax;
-    private Set<ProductResponse> productSet;
+
     public BusinessResponse(Business business) {
         this.id = business.getId();
         this.name = business.getName();
         this.about = business.getAbout();
         this.tax = business.getTax();
-        this.productSet = business.getProductSet().stream().map(
-                product -> new ProductResponse(product)).collect(Collectors.toSet());
     }
 }
