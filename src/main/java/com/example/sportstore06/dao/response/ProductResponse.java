0c8eac559df0137735dd477699440bce6f2421e0
tuyetson2509 <104404883+tuyetson2509@users.noreply.github.com>
2024-01-02 11:36:25 +0700

    Update ProductResponse.java

--- src/main/java/com/example/sportstore06/dao/response/ProductResponse.java
+++ src/main/java/com/example/sportstore06/dao/response/ProductResponse.java
@@ -27,55 +27,18 @@ public class ProductResponse {
         this.id = product.getId();
         this.id_product_information = product.getProductInfo() != null ? product.getProductInfo().getId() : null;
         this.name_product_information = product.getProductInfo() != null ? product.getProductInfo().getName() : null;
-        this.image_product_information = "";
+        this.image_product_information = null;
         this.price = product.getPrice();
         this.quantity = product.getQuantity();
         this.size = product.getSize();
         List<Image> list = product.getProductInfo().getImageSet().stream().toList();
-     package com.example.sportstore06.dao.response;
-
-import com.example.sportstore06.entity.Image;
-import com.example.sportstore06.entity.Product;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.util.List;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class ProductResponse {
-    private int id;
-    private Integer id_product_information;
-    private String name_product_information;
-    private String image_product_information;
-    private Long price;
-    private SaleResponse sale;
-    private Integer quantity;
-    private String size;
-
-    public ProductResponse(Product product) {
-        this.id = product.getId();
-        this.id_product_information = product.getProductInfo() != null ? product.getProductInfo().getId() : null;
-        this.name_product_information = product.getProductInfo() != null ? product.getProductInfo().getName() : null;
-        this.image_product_information = "";
-        this.price = product.getPrice();
-        this.quantity = product.getQuantity();
-        this.size = product.getSize();
-        List<Image> list = product.getProductInfo().getImageSet().stream().toList();
-           for (Image image : list) {
+        for (Image image : list) {
             if (image.getIs_main() != null) {
                 if (image.getIs_main()) {
                     this.image_product_information = image.getUrl();
                 }
             }
         }
-        this.sale = product.getProductInfo().getSale() != null ? new SaleResponse(product.getProductInfo().getSale()) : null;
-    }
-}
         this.sale = product.getProductInfo().getSale() != null ? new SaleResponse(product.getProductInfo().getSale()) : null;
     }
 }
