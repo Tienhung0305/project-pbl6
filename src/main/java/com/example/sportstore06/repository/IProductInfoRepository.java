087a0fec5522ed664ef155097d98b170fecb784e
tuyetson2509 <104404883+tuyetson2509@users.noreply.github.com>
2024-01-04 01:29:58 +0700

    Update IProductInfoRepository.java

--- src/main/java/com/example/sportstore06/repository/IProductInfoRepository.java
+++ src/main/java/com/example/sportstore06/repository/IProductInfoRepository.java
@@ -21,7 +21,7 @@ public interface IProductInfoRepository extends JpaRepository<ProductInfo,Intege
     Page<ProductInfo> findByCategory(Pageable pageable, List<Integer> categoryIds);
     @Query("SELECT i FROM ProductInfo i JOIN i.categorySet c WHERE i.state = :state AND c.id IN (:categoryIds)")
     Page<ProductInfo> findByCategory(Pageable pageable, Integer state, List<Integer> categoryIds);
-    @Query("SELECT i FROM ProductInfo i JOIN i.categorySet c WHERE c.id IN (:categoryIds) AND i.business.user.state = :state_business")
+    @Query("SELECT i FROM ProductInfo i JOIN i.categorySet c WHERE i.state = :state AND c.id IN (:categoryIds) AND i.business.user.state = :state_business")
     Page<ProductInfo> findByCategory(Pageable pageable, Integer state, List<Integer> categoryIds, Integer state_business);
     @Query("SELECT i FROM ProductInfo i WHERE i.state = :state AND i.business.id = :id_business")
     Page<ProductInfo> findByBusiness(Pageable pageable, Integer state, Integer id_business);
@@ -33,7 +33,6 @@ public interface IProductInfoRepository extends JpaRepository<ProductInfo,Intege
     Page<ProductInfo> findBySale(Pageable pageable, Integer id_sale);
     @Query("SELECT i FROM ProductInfo i WHERE i.sale.id = :id_sale AND i.business.user.state = :state_business")
     Page<ProductInfo> findBySale(Pageable pageable, Integer id_sale, Integer state, Integer state_business);
-
     @Query("SELECT i FROM ProductInfo i WHERE i.sale.discount = :discount")
     Page<ProductInfo> findBySaleDiscount(Pageable pageable, Long discount);
     @Query("SELECT i FROM ProductInfo i WHERE i.state = :state AND i.sale.discount = :discount")
