1e64a81a3862a633952e6c97fd2f28026c239dcc
tuyetson2509 <104404883+tuyetson2509@users.noreply.github.com>
2024-01-05 19:46:40 +0700

    Update Image.java

--- src/main/java/com/example/sportstore06/entity/Image.java
+++ src/main/java/com/example/sportstore06/entity/Image.java
@@ -19,7 +19,7 @@ public class Image {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
-    @Column(columnDefinition = "BOOLEAN")
+    @Column(columnDefinition = "boolean default false")
     private Boolean is_main;
     private Timestamp created_at;
     private Timestamp updated_at;
